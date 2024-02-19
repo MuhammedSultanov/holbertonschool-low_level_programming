efe16e0b90bd7575e536dffeeb19406e2ad7e1a3
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-18 23:59:35 -0800

    betty clean code

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
