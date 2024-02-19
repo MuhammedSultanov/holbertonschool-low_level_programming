afcd9f92ff787a0998cc96aae171fb660ca21a61
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-19 08:16:31 -0800

    last c advanced task

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: this code for print size of various
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fgets("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+	return (1);
+}
