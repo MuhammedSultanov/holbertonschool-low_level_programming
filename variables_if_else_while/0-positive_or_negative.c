1dcce1276816018ef4adbc6e645682acd478caf5
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-19 22:11:02 -0800

    if a>b: print("a is positive")

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{printf("%d",n," is positive");
+	{printf("%d is positive", n);
 	}
 	else if (n < 0)
-	{printf("%d",n," is negative");
+	{printf("%d is negative", n);
 	}
 	else
-	{printf("%d",n," is zero");
+	{printf("%d is zero", n);
 	}
 	return (0);
 }
