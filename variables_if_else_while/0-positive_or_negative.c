2dd02453919ca7b78290dc01bfc9244fd355fb1c
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-19 21:33:28 -0800

    if a>b:

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -10,10 +11,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n>0){
-		printf("is positive")};
+		printf("is positive");}
 	else if(n<0){
-		printf("is negative")};
+		printf("is negative");}
 	else{
-		printf("is zero")};
+		printf("is zero");}
 	return (0);
 }
