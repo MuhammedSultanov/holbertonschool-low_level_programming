4876cfdbb4a67fd7f3d1fe41dcf2cb6ed94fa5de
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-20 09:46:55 -0800

    last advanced task 00 00

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -20,12 +20,12 @@ for (k = '0'; k <= '9'; k++)
 {
 putchar(x);
 putchar(y);
-if (x != '0' && y != '0' && z != '0' && k != '0')
+if (!(x == '0' && y == '0' && z == '0' && k == '0'))
 {
 putchar(' ');
 putchar(z);
 putchar(k);
-putchar(',');
-putchar(' '); }}}}}
+{putchar(',');
+putchar(' '); }}}}}}
 putchar('\n');
 return (0); }
