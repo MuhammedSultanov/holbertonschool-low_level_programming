9079c5f156becccbf230dabe8873d3ee8337b563
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-20 07:41:30 -0800

    tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char x = 'z';
+for (x = 'z'; x <= 'a'; x--)
+	putchar(x);
+putchar('\n');
+return (0); }
