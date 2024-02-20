616fb335fb498db5c00f8dcbf5c3cc991b60c761
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-20 08:04:54 -0800

    cisfun

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,7 +11,8 @@ int main(void)
 {char x = '0';
 for (x = '0'; x <= '9'; x++)
 {	putchar(x);
-	putchar(',');
-	putchar(' '); }
+	if (x != '9')
+{	putchar(',');
+	putchar(' '); }}
 putchar('\n');
 return (0); }
