cd2346460471f5137d8b7072f610f78df313b4eb
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-20 08:47:39 -0800

    uniq digits

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,12 @@ int main(void)
 int y;
 for (x = '0'; x <= '9'; x++)
 {
-for (y = '0'; y <= '9'; y++)
+for (y = x + 1; y <= '9'; y++)
 {putchar(x);
 putchar(y);
-if (x != '9' || y != '9')
-{putchar(',');
+if (x != '8' || y != '9')
+{
+putchar(',');
 putchar(' '); }}}
 putchar('\n');
 return (0); }
