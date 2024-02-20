f69185a4e4195c1a6fb8c68a6ccfc36fcfef1ca2
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-19 21:37:30 -0800

    if a>b:

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */ 
 int main(void)
 {
 	int n;
@@ -11,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n>0){
-		printf("is positive");}
+		printf(" is positive");}
 	else if(n<0){
-		printf("is negative");}
+		printf(" is negative");}
 	else{
-		printf("is zero");}
+		printf(" is zero");}
 	return (0);
 }
