a9c57de4eb29e4868e50a1f8b3efe027f34e1822
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-19 23:34:23 -0800

    alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {char x, y;
-	for (x = 'a'; x <= 'z'; x++)
-	{putchar(x); }
+for (x = 'a'; x <= 'z'; x++)
+{putchar(x); }
 for (x = 'A'; y <= 'Z'; y++)
-	{putchar(y); }
+{putchar(y); }
 putchar('\n');
 
 return (0); }
