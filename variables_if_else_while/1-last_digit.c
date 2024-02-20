1df1e223f3fab63f162c568a2974f6349cd0460e
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-19 22:41:23 -0800

    last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	a = n % 10;
 	if (a > 5)
-	{printf("%d %d and is greater than 5", n, a); }
+	{printf("Last digit of %d %d and is greater than 5", n, a); }
 	else if (a < 6 && a != 0)
-	{printf("%d %d and is less than 6 and not 0", n, a); }
+	{printf("Last digit of %d %d and is less than 6 and not 0", n, a); }
 	else
-	{printf("%d %d and is 0", n, a); }
+	{printf("Last digit of %d %d and is 0", n, a); }
 	return (0);
 }
