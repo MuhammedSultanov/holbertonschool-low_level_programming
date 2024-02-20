91c75db7fc098d07623680f122d3594e4174539e
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-20 07:46:47 -0800

    0-9 && a-f

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char x;
+char y;
+for (x = '1'; x <= '9'; x++)
+{putchar(x); }
+for (y = 'a'; y <= 'f'; y++)
+{putchar(y); }
+putchar('\n');
+return (0); }
