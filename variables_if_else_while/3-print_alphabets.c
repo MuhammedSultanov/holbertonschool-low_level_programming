cd835c31ab555b541ea310a8f699577b61b54132
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-19 23:06:55 -0800

    mega alphabet print

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char x = 'a';
+for (x = 'a'; x <= 'z'; x++)
+char y = 'A';
+for (x = 'A'; y <= 'Z'; y++)
+	putchar(x);
+	putchar(y);
+putchar('$');
+putchar('\n');
+
+return (0); }
