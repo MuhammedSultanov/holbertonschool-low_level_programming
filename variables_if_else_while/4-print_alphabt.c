bbb4b4b4937f03840dadbe6f5ee190e0312a63e3
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-19 23:59:54 -0800

    != eq

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char x;
+char y;
+char k;
+for (x = 'a'; x <= 'z'; x++)
+{
+if (x != 'q' && x != 'e')
+{putchar(x); }}
+putchar('\n');
+return (0); }
