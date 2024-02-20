f5cfb9529033980eba19ab7c4086a551395b3317
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-20 07:34:30 -0800

    0-9 without char

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int x = '0';
+for (x = '0'; x <= '9'; x++)
+	putchar(x);
+putchar('\n');
+return (0); }
