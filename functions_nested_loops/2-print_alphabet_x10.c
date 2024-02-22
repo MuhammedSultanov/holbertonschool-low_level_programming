fc82ed171daa36e881da26a40e4eb85fb7f378e4
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-22 08:06:36 +0000

    task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+
+	while (a[i])
+	{
+		_putchar(a[i]);
+		i++;
+	}
+	for ( a = 1; 1 >= 10; i++)
+	{ _putchar(a[i]);
+}
