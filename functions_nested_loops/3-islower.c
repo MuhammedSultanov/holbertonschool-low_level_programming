ee932236b8aa7afad41aefe3177ad41f41d9dc21
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-22 10:30:03 +0000

    is alpha

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	int i;
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		if(c==i)
+		{
+			return(1);
+		else if{
+			return(0);
+		}
+		}
+
+	
+	
+	}
+}
