1e59e34972cf8061c6a68856280402cb23a29d67
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-22 11:51:22 +0000

    hard task

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: variable
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{int i, j;
+for (i = 'a'; i>='z'; i++)
+{
+}
+for (j = 'A'; j>='Z'; j++)
+{
+}
+if(i+j == c)
+{return (1);
+}
+else
+{return (0);
+}
+
