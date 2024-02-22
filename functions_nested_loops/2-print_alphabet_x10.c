103707c5dee783c8763a3a2aeb7b0032ad42457f
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-22 08:56:15 +0000

    hi hard taskk

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,18 @@ void print_alphabet_x10(void)
 {
 	int j;
 	j = 0;
-	while(j<11)
+	while(j < 10)
 	{
+		
+		char a[] = "abcdefghijklmnopqrstuvwxyz\n";
+		int i = 0;
+		
+		while (a[i])
+		{
+			_putchar(a[i]);
+			i++;
+		}
 		j++;
-	char a[] = "abcdefghijklmnopqrstuvwxyz\n";
-	int i = 0;
-
-	while (a[i])
-	{
-		_putchar(a[i]);
-		i++;
-	}
-	
+		
 	}
 }
