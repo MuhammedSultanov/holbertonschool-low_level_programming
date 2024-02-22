eadfd71522298ce499e9bd94c12104220b88b1c1
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-22 10:43:44 +0000

    task islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -10,13 +10,14 @@ int _islower(int c)
 	int i;
 	for(i = 'a'; i <= 'z'; i++)
 	{
-		if(c==i)
+		if(c == i)
 		{
-			return(1);
+			return (1);
 		}
 		else
 		{
-			return(0);
+			return (0);
 		}
 	}
+
 }
