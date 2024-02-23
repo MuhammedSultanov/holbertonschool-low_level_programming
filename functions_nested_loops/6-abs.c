b751050dc7973dc4b38828a08b9a2677f910087f
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-23 20:17:41 +0400

    abs first try

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: variable
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+_putchar('n');
+}
+else if (n == 0)
+{
+_putchar('0');
+}
+else if (n < 0)
+{
+n=0-n
+_putchar('n');
+}
+return (0);
+}
