ebcd46a45b1028d6daac750daf0e6092b0b3402b
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-24 01:48:45 +0400

    24 hrs

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,28 +6,37 @@
  */
 void jack_bauer(void)
 {
-int i = 0, j = 0, o = 0, g = 0;
+	int i = 0, j = 0;
 
-for (i = 0; i <= 2 ; i++)
-{
-for (j = 0; j <= 9 ; j++)
-{
-for (o = 0; o <= 5 ; o++)
-{
-for (g = 0; g <= 9 ; g++)
-{
-_putchar(i + '0');
-_putchar(j + '0');
-_putchar(':');
-_putchar(o + '0');
-_putchar(g + '0');
-if ((10 * i + j == 23) && (10 * o + g == 59))
-{ return;
-}
-_putchar('\n');
-}
-}
-}
-}
-_putchar('\n');
+	while (i < 24)
+	{
+		while (j < 60)
+		{
+			if (i < 10)
+			{
+				_putchar ('0');
+				_putchar (i % 10 + '0');
+			}
+			else
+			{
+				_putchar (i / 10 + '0');
+				_putchar (i % 10 + '0');
+			}
+			putchar (':');
+			if (j < 10)
+			{
+				_putchar ('0');
+				_putchar (j % 10 + '0');
+			}
+			else
+			{
+				_putchar (j / 10 + '0');
+				_putchar (j % 10 + '0');
+			}
+			_putchar ('\n');
+			j++;
+		}
+		j = 0;
+		i++;
+	}
 }
