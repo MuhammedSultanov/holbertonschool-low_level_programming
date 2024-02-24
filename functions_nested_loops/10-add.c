b5239f2a678e8d0f9b5c0dc9efbcb801e6a05dc3
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-24 13:09:17 +0400

    add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
+ * add - Entry point
  *
  * Return: Always 0 (Success)
  */
 int add(int n, int m)
 {
-int v;
+int v, l;
 v = n + m;
+l = (v % 10) + '0'
+int * returnVvar = l
 
 _putchar(v / 10 + '0');
 _putchar(v % 10 + '0');
-return v % 10 + '0';
+return returnVvar;
 }
-
