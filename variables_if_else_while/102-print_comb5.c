1a7d9331772d34236bfc6b05e73ee965353bbbc2
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-24 23:24:08 +0400

    102 task

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -6,30 +6,32 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{int x;
-int y;
-int z;
-int k;
-int u;
-int t;
-for (x = '0'; x <= '9'; x++)
 {
-for (y = '0' ; y <= '9'; y++)
-{
-for (z = '0'; z <= '9'; z++)
-{
-for (k = '0'; k <= '9'; k++)
-{
-u=10*x + y ;
-t=10*z + k ;
-if (!(u == t))
-{
-putchar(x);
-putchar(y);
-putchar(' ');
-putchar(z);
-putchar(k);
-{putchar(',');
-putchar(' '); }}}}}}
-putchar('\n');
-return (0); }
+	int i, j, n, m;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (n = i; n <= 57; n++)
+			{
+				for (m = 49; m <= 57; m++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(' ');
+					_putchar(n);
+					_putchar(m);
+					if (i != 57 || j != 56 || n != 57 || m != 57)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				m = 48;
+			}
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
