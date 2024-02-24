e601aefd6824ecef90090400d0d913ffdb38a59d
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-24 12:27:54 +0400

    matris

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,35 +7,39 @@
  */
 void times_table(void)
 {
-int i = 0, j = 0;
-while (i < 10)
-{
-while (j < 10)
-{
-if (i * j < 10)
-{
-_putchar((i * j) % 10 + '0');
-}
-else
-{
-_putchar((i * j) / 10 + '0');
-_putchar((i * j) % 10 + '0');
-}
-if (j != 9)
-_putchar(',');
-if (i * (j + 1) <= 9)
-{
-_putchar(' ');
-_putchar(' ');
-}
-else
-{
-_putchar(' ');
-}
-}
-j++;
-}
-_putchar('\n');
-i++;
-j = 0;
+    int i = 0, j = 0;
+    
+    while (i < 10)
+    {
+        while (j < 10)
+        {
+            if (i * j < 10)
+            {
+                _putchar((i * j) % 10 + '0');
+                
+            }
+            else
+            {
+                _putchar((i * j) / 10 + '0');
+                _putchar((i * j) % 10 + '0');
+            }
+            if(j != 9)
+            {
+                _putchar(',');
+                if(i * (j + 1) <= 9)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                else
+                {
+                    _putchar(' ');
+                }
+            }
+            j++;
+        }
+        _putchar('\n');
+        i++;
+        j = 0;
+    }
 }
