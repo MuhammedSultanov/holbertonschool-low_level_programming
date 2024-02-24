c0dc84eddb011783c058d80c4bc1009617ba487c
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-24 16:36:43 +0400

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -17,8 +17,7 @@ void print_to_98(int n)
         else{
             printf("%d, ", n);
             n++;
-            
-        }
+            }
     }
-    printf("98");
+    printf("%d\n", n);
 }
