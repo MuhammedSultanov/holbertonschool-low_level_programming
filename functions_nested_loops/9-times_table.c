07c47026ca98f2c1005b391f051e2b0cd8cb8778
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-24 11:56:44 +0400

    matris

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+    int i = 0, j = 0;
+    
+    while(i<10)
+    {
+        while(j<10)
+        {
+            if (i*j < 10)
+            {
+                _putchar(' ');
+                _putchar((i*j)%10+ '0');
+            }
+            else{
+                _putchar((i*j)/10+ '0');
+                _putchar((i*j)%10+ '0');
+            }
+            j++;
+            if(j !=10)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+        i++;
+        j = 0;
+ i   }
+}
