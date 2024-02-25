e9e9caaff270c04d8a5acdb79e3380e52b0f1e23
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-25 15:32:34 +0400

    fi^2 = fi + 1

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-    int a, b, c, i;
-    a = 1;
-    b = 1;
-    for (i = 1; i<51; i++)
-    {
-        a = b;
-        b = c;
-        c = a + b;
-        printf ("%d, ", c);
-    }
-    printf ("\n");
-return(0);
+int a, b, c, i;
+a = 1;
+b = 1;
+for (i = 1; i<51; i++)
+{
+a = b;
+b = c;
+c = a + b;
+printf ("%d, ", c);
 }
+printf ("\n");
+return(0); }
