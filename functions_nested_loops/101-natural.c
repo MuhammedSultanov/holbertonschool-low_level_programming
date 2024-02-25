ad79c52b46f74ac8597d4677d3373ebf590cc99e
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-25 15:03:31 +0400

    fixed betty error

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main() 
+int main(void)
+{int i, c;
+c = 0;
+for (i = 0; i < 1024; i++)
 {
-    int i,c;
-    c = 0;
-    for (i = 0; i<1024; i++)
-    {
-        if((i % 5 == 0) || (i % 3 == 0))
-        {
-            c = c + i;
-        }
-    }
-    printf("%d\n",c);
-return(0);
+if ((i % 5 == 0) || (i % 3 == 0))
+{
+c = c + i;
+}
+}
+printf("%d\n", c);
+return (0);
 }
