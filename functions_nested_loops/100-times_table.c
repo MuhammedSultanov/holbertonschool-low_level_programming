0538999460d1d9561a0a1f5d8856a7a72948c36d
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-25 13:19:04 +0400

    fixed betty style and added des.

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,60 +1,39 @@
 #include "main.h"
-
 /**
  * print_times_table - Entry point
+ * @n: variable
  *
  * Return: Always 0 (Success)
  */
 void print_times_table(int n)
+{int i = 0, j = 0;
+if (!(n < 0 || n > 15))
 {
-int i = 0, j = 0;
-    if (!(n < 0 || n > 15))
-    {
-        while (i <= n)
-        {
-            while (j <= n)
-            {
-                if (i * j <= 9)
-                {
-                    _putchar((i * j) % 10 + '0');
-                    
-                }
-                else if (i * j <= 99)
-                {
-                    _putchar((i * j) / 10 + '0');
-                    _putchar((i * j) % 10 + '0');
-                    
-                }
-                else
-                {   _putchar((i * j) / 100 + '0');
-                    _putchar(((i * j) / 10) % 10 + '0');
-                    _putchar((i * j) % 10 + '0');
-                    
-                }    
-                if (j != n)
-                {
-                    _putchar(',');
-                    if (i * (j + 1) <= 9)
-                    {
-                        _putchar(' ');
-                        _putchar(' ');
-                        _putchar(' ');
-                    }
-                    else if (i * (j + 1) >= 100)
-                    {
-                        _putchar(' ');
-                    }
-                    else
-                    {
-                        _putchar(' ');
-                        _putchar(' ');
-                    }
-                    
-                }j++;
-            }
-            _putchar('\n');
-            i++;
-            j = 0;
-        }
-    }
-}
+while (i <= n)
+{
+while (j <= n)
+{
+if (i * j <= 9)
+{_putchar((i * j) % 10 + '0'); }
+else if (i * j <= 99)
+{_putchar((i * j) / 10 + '0');
+_putchar((i * j) % 10 + '0'); }
+else
+{_putchar((i * j) / 100 + '0');
+_putchar(((i * j) / 10) % 10 + '0');
+_putchar((i * j) % 10 + '0'); }
+if (j != n)
+{_putchar(',');
+if (i * (j + 1) <= 9)
+{_putchar(' ');
+_putchar(' ');
+_putchar(' '); }
+else if (i * (j + 1) >= 100)
+{_putchar(' '); }
+else
+{_putchar(' ');
+_putchar(' '); }}
+j++; }
+_putchar('\n');
+i++;
+j = 0; }}}
