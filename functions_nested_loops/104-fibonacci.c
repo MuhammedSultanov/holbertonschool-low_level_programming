a160fd766f73f077073dd6c809a74deeb9a43956
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-25 23:11:52 +0400

    10 000 000 000

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -5,28 +5,38 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{int i;
-unsigned long a, b, c;
-a = 1;
-b = 1;
-printf("%lu, ", a);
-for (i = 2; i < 99; i++)
 {
-c = a + b;
-if (i <= 93)
+int i;
+unsigned long n1, n2, n1h1, n1h2, sum, n2h1, n2h2, n3, n4;
+n1 = 0;
+n2 = 1;
+for (i = 0; i < 92; i++)
 {
-printf("%lu", c);
+sum = n1 + n2;
+printf("%lu, ", sum);
+n1 = n2;
+n2 = sum;
 }
-else
+n1h1 = n1 / 10000000000;
+n2h1 = n2 / 10000000000;
+n1h2 = n1 % 10000000000;
+n2h2 = n2 % 10000000000;
+for (i = 93; i < 99; i++)
 {
-printf("%lu%010lu", c / 1000000000, c % 1000000000);
+h3 = n1h1 + n2h1;
+h4 = n1h2 + n2h2;
+if (n1h2 + n2h2 > 9999999999)
+{
+h3 += 1;
+h4 %= 10000000000;
 }
-if (i < 98)
-{
+printf("%lu%lu", h3, h4);
+if (i != 98)
 printf(", ");
-a = b;
-b = c;
-}
+n1h1 = n2h1;
+n1h2 = n2h2;
+n2h1 = h3;
+n2h2 = h4;
 }
 printf("\n");
 return (0);
