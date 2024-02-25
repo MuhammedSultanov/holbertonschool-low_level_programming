bbc29273bee1faaf5fd35c10f5396d3c7c9b5903
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-25 19:11:24 +0400

    last task

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -5,29 +5,28 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{int i;
-unsigned long a, b, c;
-a = 1;
-b = 1;
-printf("%lu, ", a);
-for (i = 1; i < 98; i++)
 {
-c = a + b;
-if (c > 10000000000) 
-{
-printf("%lu%010lu", c / 10000000000, c % 10000000000);    
-}
-else
-{
-printf("%lu", c);    
-}
-if (i < 97)
-{
-printf(", ");
-a = b;
-b = c;
-}
-}
-printf("\n");
-return (0);
+    int i;
+    unsigned long a, b, c;
+    a = 1;
+    b = 1;
+    printf("%lu, ", a);
+
+    for (i = 1; i < 98; i++) {
+        c = a + b;
+        if (c > 1000000000) {
+            printf("%lu%010lu", c / 1000000000, c % 1000000000);
+        } else {
+            printf("%lu", c);
+        }
+
+        if (i < 97) {
+            printf(", ");
+            a = b;
+            b = c;
+        }
+    }
+
+    printf("\n");
+    return 0;
 }
