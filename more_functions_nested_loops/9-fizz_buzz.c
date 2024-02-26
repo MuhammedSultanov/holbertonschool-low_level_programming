1640eded8c3c27f9596de88d42f79ca20e384c51
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 20:09:52 +0400

    fizzzzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @size: var
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+    int i;
+    
+    for (i = 1; i < 101; i++)
+    {
+            if (i % 3 == 0)
+            {
+                printf("Fizz ");
+            }
+            else if (i % 5 == 0)
+            {
+                printf("Buzz ");
+            }
+            else
+            {
+                printf("%d ", i);
+            }
+        
+
+    }
+    return (0);
+}
