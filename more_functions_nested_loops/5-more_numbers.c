0c9dce19ab929eab5c4f50c680a6d3279e03c822
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 06:51:17 +0000

    10x 1-14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,9 @@ void more_numbers(void)
 		{
 			if (j > 10)
 			{
-				_putchar(i / 10 + '0');
+				_putchar(j / 10 + '0');
 			}
-			_putchar(i % 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
