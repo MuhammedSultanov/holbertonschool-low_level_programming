82b61b2d7b149d01d2c9d938ad873302e87899a3
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 07:49:38 +0000

    print line

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * print_diagonal - Entry point
- * @n: var
+ * print_square - Entry point
+ * @size: var
  * Return: Always 0 (Success)
  */
-void print_diagonal(int n)
+void print_square(int size)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
