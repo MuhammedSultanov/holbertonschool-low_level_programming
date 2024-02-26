f52d1330009207f81b37384e720325b89202398f
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 07:47:43 +0000

    print line

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry point
+ * @n: var
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
