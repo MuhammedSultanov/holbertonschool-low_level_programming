a08cb8b86c4cf70333af48097a5358be0b5c7165
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 05:40:43 +0000

    is upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -10,16 +10,10 @@ int _isupper(int c)
 {
 	if (c >= 65 || c <= 90)
 	{
-		_putchar('c');
-		_putchar(':');
-		_putchar(' ');
 		return (1);
 	}
 	else
 	{
-		_putchar('c');
-		_putchar(':');
-		_putchar(' ');
-		 return (0);
+		return (0);
 	}
 }
