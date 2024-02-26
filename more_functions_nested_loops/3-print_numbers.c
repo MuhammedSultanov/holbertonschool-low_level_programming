dd9e2de0500c049957d592f414f6d56d398cba4c
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 06:13:47 +0000

    multiplies

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c: variable
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	int i;
+	for (i = 0; i > 10; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+	return;
+}
