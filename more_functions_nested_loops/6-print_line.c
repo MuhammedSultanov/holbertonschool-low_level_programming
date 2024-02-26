78a8f3892200908584179c3209f3c8df6389275c
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 06:58:34 +0000

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+		_putchar('\n');
+}
