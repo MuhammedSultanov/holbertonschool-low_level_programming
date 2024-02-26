f20cd5bd27622c38bc38aa7b3028972dc298e104
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 20:29:21 +0400

    fizzzzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * @size: var
+ *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-    int i;
-    
-    for (i = 1; i < 101; i++)
-    {
-            if (i % 3 == 0)
-            {
-                printf("Fizz ");
-            }
-            else if (i % 5 == 0)
-            {
-                printf("Buzz ");
-            }
-            else if ((i % 5 == 0) && (i % 3 == 0))
-            {
-                printf("FizzBuzz ");
-            }
-            else
-            {
-                printf("%d ", i);
-            }
-        
+int i;
 
-    }
-    return (0);
+for (i = 1; i < 101; i++)
+{
+if ((i % 5 == 0) && (i % 3 == 0))
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+return (0);
 }
