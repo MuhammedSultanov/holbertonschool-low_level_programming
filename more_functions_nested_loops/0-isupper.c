bf584f8a66d8195a7ea4df4289469319501688e2
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-26 05:13:34 +0000

    is upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @c - var
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (i >= 65 || i <= 90)
+	{
+		_putchar('c');
+		_putchar(':');
+		_putchar(' ');
+		return (1);
+	}
+	else
+	{
+		_putchar('c');
+		_putchar(':');
+		_putchar(' ');
+	return (0);
+}
