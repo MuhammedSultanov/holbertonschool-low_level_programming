8ff9d202fc403582b64f884b1a08aec171cea0bc
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-27 06:17:04 +0000

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,26 +7,25 @@
  */
 void print_triangle(int size)
 {
-        int i,  j, k;
-        
+	int i, j, k;
+
         if (size > 0)
-        {
-            for (i = 0; i < size ; i++)
-            {
-                    for (j = i; j < size - 1; j++)
-                    {
-                            _putchar(' ');
-                    }
-                    for (k = i + 1; k > 0; k--)
-                    {
-                            _putchar('#');
-                    }
-                    
-                    _putchar('\n');
-            }
-        }
-        else
-        {
+	{
+		for (i = 0; i < size ; i++)
+		{
+			for (j = i; j < size - 1; j++)
+			{
+				_putchar(' ');
+			for (k = i + 1; k > 0; k--)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			}
+		}
+	}
+	else
+	{
 		_putchar('\n');
-        }
+	}
 }
