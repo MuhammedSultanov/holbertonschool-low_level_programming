095606512cabf696dad2040a1d2c6f3d6cd586e5
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-27 06:22:45 +0000

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -21,11 +21,11 @@ void print_triangle(int size)
 			{
 				_putchar('#');
                         }
-			putchar('\n');
+			_putchar('\n');
 		}
         }
         else
         {
-		putchar('\n');
+		_putchar('\n');
         }
 }
