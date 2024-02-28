68c35efd417bebda99fbe01161960be8c93b4485
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-28 21:32:32 +0400

    fixed betty style and _putchar

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,14 @@
  */
 void print_rev(char *s)
 {
-  int len = 0, i;
-  while (s[len])
-  {
-    len++;
-  }
-  for (i = len - 1; i >= 0; i--)
-  {
-    putchar(s[i]);
-  }
+int len = 0, i;
+while (s[len])
+{
+len++;
+}
+for (i = len - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
