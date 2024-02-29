80f2ed971a7591ffc00388d9d74721a470554d1b
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-29 18:48:38 +0400

    ;=;

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reset_to_98 - check the code
+ * @n: - input of function
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+  int i;
+  for (i = 0; src[i] != '\0'; i++ )
+{
+  dest[i] = src[i];
+
+}
+putchar('\n');
+return(dest);
+}
