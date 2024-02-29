5df3822aeaf59ac594a9a644850335300ea2355e
Muhammed Sultanov <9073@holbertonstudents.com>
2024-02-29 11:06:54 +0000

    array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - Entry point
+ * @n: description for n
+ * @a: var a
+ *
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+  int i = 0;
+  for (i = 0; i < n; i++)
+  {
+    printf("%d", a[i]);
+    if (i != n - 1)
+    {
+    printf(", ");
+    }
+  }
+  printf("\n");
+}
