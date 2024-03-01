ca20cd75cabf2273f0068841f2339387af700dea
Muhammed Sultanov <9073@holbertonstudents.com~>
2024-03-01 07:09:51 +0000

    ncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - check the code
+ * @dest: - input of function
+ * @src: - src
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int a = 0, c = 0;
+
+for (a = 0; dest[a] != '\0'; a++)
+{
+}
+for (c = 0; c < n; c++)
+{
+dest[a + c] = src[c];
+if (src[c] == '\0')
+{
+break;
+}
+}
+return (dest);
+}
