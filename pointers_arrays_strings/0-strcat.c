0b3531ab8b6920798588348b4f5ac3929bada855
Muhammed Sultanov <9073@holbertonstudents.com~>
2024-03-01 06:08:01 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcat - check the code
+ * @dest: - input of function
+ * @src: - src
+ *
+ */
+{
+char *_strcat(char *dest, char *src)
+int a = 0, b = 0;
+
+for(a=0; dest[a]!='\0'; ++a)
+for(b=0; src[b]!='\0'; ++b, ++a)
+{
+dest[a]=src[b];
+}
+dest[a]='\0';
+return(dest);
+}
