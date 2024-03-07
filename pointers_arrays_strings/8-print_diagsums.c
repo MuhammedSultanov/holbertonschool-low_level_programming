86407e6ef1bbcfbb12ab4e5a34bf94fa588d96a8
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-07 15:31:55 +0400

    diagonal

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,9 +13,9 @@ for (i = 0; i < size; i++)
 sum1 = sum1 + a[i * size + i];
 sum2 = sum2 + a[i * size + (size - 1 - i)];
 }
-_putchar(sum1);
+_putchar(sum1 + '0');
 _putchar(',');
 _putchar(' ');
-_putchar(sum2);
+_putchar(sum2 + '0');
 _putchar('\n');
 }
