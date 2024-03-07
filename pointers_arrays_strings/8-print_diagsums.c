06aaebe34f01177d46578c11693ec357657155cb
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-07 15:35:40 +0400

    diagonal

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,7 +7,8 @@
  */
 void print_diagsums(int *a, int size)
 {
-unsigned int i, sum1 = 0, sum2 = 0;
+int i;
+unsigned int sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
 sum1 = sum1 + a[i * size + i];
