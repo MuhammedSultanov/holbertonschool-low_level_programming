0376aa09d165f044500071d5aad672eb56538af5
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-07 15:52:30 +0400

    stdio

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,8 +8,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-unsigned int i, j, sum1 = 0, sum2 = 0;
-
+int i, j, sum1 = 0, sum2 = 0;
 for (i = 0; i <= (size * size); i = i + size + 1)
 {
 sum1 = sum1 + a[i];
