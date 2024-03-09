3418d1add5943df65336d394b49fa1f3f7af19fc
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-09 17:11:41 +0400

    multiple

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -16,8 +16,8 @@ if (argc != 3)
 printf("Error\n");
 return (1);
 }
-mul = atoi(argv[1]) * atoi(argv[2])
-printf("%d\n", mul)
+mul = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", mul);
 return (0);
 }
 
