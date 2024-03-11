c7f6ef3610553480ed0dfd8d9554e9b240ec6612
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-11 13:53:06 +0000

    fixed errors

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return(NULL);
 	}
-	array = malloc(sizeof(char) * size);
+	array = malloc(sizeof(int) * size);
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
