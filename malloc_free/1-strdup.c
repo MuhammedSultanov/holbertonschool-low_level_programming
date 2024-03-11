fc0aca097350ab9e94aff4c1fe908ef640814bc0
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-11 14:33:55 +0000

    just file save

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - functions are used to duplicate a string
+ *
+ * @str: - string
+ *
+ * Return: always 0
+ */
+char *_strdup(char *str)
+{
+
+}
