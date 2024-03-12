01f8e5d8d45d6279bcccd86168e602820a46990b
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-12 11:57:29 +0000

    task1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,5 +10,25 @@
  */
 char *_strdup(char *str)
 {
+	char *array;
+	int i = 0, len = 0;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	array = malloc((len + 1) * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+	{
+		array[i] = str[i];
+	}
+	return (array);
 }
