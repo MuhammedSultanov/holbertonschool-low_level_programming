82d8eeb8225ec19cd38fa8b066afdae84a1bb0ea
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-15 15:29:48 +0400

    task 0

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - prototype for concatenates two strings
+ *
+ * @s1: - string  
+ * @s2: - string
+ * @n: - n
+ *
+ * Return: always 0
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
