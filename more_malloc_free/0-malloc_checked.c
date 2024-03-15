aaea7960dfd27bcbff2e21eb36ef413e2e08545d
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-15 16:23:49 +0400

    pointer type

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
 /**
  * malloc_checked - prototype for allocates memory using malloc
  * @b: - b
