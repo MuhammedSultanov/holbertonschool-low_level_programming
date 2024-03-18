0043a6591f35faa7719d4865e12c5722a8150e73
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-18 05:52:54 +0000

    Lisa

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,12 @@
 #include <atslib.h>
+/**
+ * struct dog - creates dog struct
+ * name: name of dog
+ * age: age of dog
+ * owner: name of owner
+ *
+ * Description: struct of dog table
+ * */
 struct dog
 {
 	char *name;
