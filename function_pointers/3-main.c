5c457eba7b4772c5670602940cd95557d35e7285
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-23 14:19:29 +0000

    some bugs fixed

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,30 +10,33 @@
  */
 int main(int argc, char *argv[])
 {
-int a, b;
-char *op;
+	int a, b;
+	char *op;
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-op = argv[2];
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
 
-if (get_op_func(op) == NULL || op[1] != '\0')
-{
-printf("Error\n");
-exit(98);
-}
-if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
-{
-printf("Error\n");
-exit(100);
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-printf("%d\n", get_op_func(op)(a, b));
-}
-return (0);
+	if ((*op == '/' && b == 0) ||
+		(*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(a, b));
+
+	return (0);
 }
