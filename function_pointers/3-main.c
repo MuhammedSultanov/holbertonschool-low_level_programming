b550f94c7ed6f52d0f0a40950f9966f723e00c54
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-23 14:14:57 +0000

    cal

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -33,7 +33,7 @@ if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
 printf("Error\n");
 exit(100);
 
-printf("%d\n", get_op_func(op)(a, b))
+printf("%d\n", get_op_func(op)(a, b));
 }
 return (0);
 }
