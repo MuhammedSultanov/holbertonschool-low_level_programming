83c62f877de6524bf88170f9eb4586f3da8b6854
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-24 19:24:31 +0400

    01001000 01000101 01001100 01001100 01001111

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,6 +1,5 @@
 section .data
-    hello_msg db 'Hello, World!', 10  ; Message to print
-    hello_len equ $ - hello_msg        ; Length of the message
+    hello_msg db 'Hello, World' 0xA ; Message to print
 
 section .text
     global _start
@@ -10,9 +9,9 @@ _start:
     mov rax, 1             ; syscall number for write
     mov rdi, 1             ; file descriptor 1 (stdout)
     mov rsi, hello_msg     ; address of the message
-    mov rdx, hello_len     ; length of the message
+    mov rdx, 13            ; length of the message
 
-    syscall
+    syscall		   ; Invoke the kernel
 
     mov rax, 60            ; syscall number for exit
     xor rdi, rdi           ; exit code 0
