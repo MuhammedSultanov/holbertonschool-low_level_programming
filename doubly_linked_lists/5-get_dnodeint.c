9f7eb5fd1f1d4ae2946199de0881946aae98f761
MuhammedSultanov <9073@holbertonstudents.com>
2024-04-03 18:19:35 +0000

    I change the increment in cs while instructions

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
  * get_dnodeint_at_index -  returns the nth node of a dlistint_t linked list
  * @head: pointer named head and this first node
  * @index: the index of the node, starting from 0
- * Return: the nth node of a linked list
+ * Return: the nth node of a linked list.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
@@ -17,12 +17,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 	while (temp != NULL)
 	{
-		temp = temp->next;
-		c++;
 		if (c == index)
 		{
 			return (temp);
 		}
+		temp = temp->next;
+		c++;
 	}
 	return (temp);
 }
