bfbfbd826f774d0b80c93ee8ba7c2211a1ac10d1
Muhammed Sultanov <9073@holbertonstudents.com>
2024-04-07 22:02:55 +0400

    fixed some bugs

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		for (len = 0; text_content[len] != '0'; len++)
+		for (len = 0; text_content[len] != '\0'; len++)
 		;
 	}
 	fd = open(filename, O_CREAT | O_RDWR);
