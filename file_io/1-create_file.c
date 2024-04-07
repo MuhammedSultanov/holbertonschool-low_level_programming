c524ca5f63c9f9946d17ac58896f56563fb97b07
Muhammed Sultanov <9073@holbertonstudents.com>
2024-04-07 21:51:31 +0400

    a function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_file - a function that creates a file.
+ * @filename: pointer
+ * @text_content:  terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int len = 0, w, r, fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (len = 0; text_content[len] != '0'; len++)
+		;
+	}
+	fd = open(filename, O_CREAT | O_RDWR);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
